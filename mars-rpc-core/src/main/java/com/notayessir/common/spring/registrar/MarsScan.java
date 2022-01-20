08507e71b7a4626efb415b24570634a756913a98
notayessir <jxh.miracle@foxmail.com>
2022-01-20 22:23:45 +0800

    feat(annotation): update scanned package name

--- mars-rpc-core/src/main/java/com/notayessir/common/spring/registrar/MarsScan.java
+++ mars-rpc-core/src/main/java/com/notayessir/common/spring/registrar/MarsScan.java
@@ -9,6 +9,6 @@ import org.springframework.context.annotation.ImportResource;
  * 仅仅对用 ComponentScan 进行包装，用于扫描该框架组件
  */
 @Configuration
-@ComponentScan("com.github.luckyMax")
+@ComponentScan("com.notayessir")
 @ImportResource("classpath:mars.xml")
 public class MarsScan {}
